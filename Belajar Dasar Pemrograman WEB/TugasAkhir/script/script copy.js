b5a53117457fb231c00cb1738ed02d0c17dd11f3
ifdall26 <lisyukri.ifdal@gmail.com>
2024-02-29 04:01:24 +0700

    menyelesaikan project tugas akhir kelas Belajar Dasar Pemrograman WEB

---
+++ Belajar Dasar Pemrograman WEB/TugasAkhir/script/script copy.js
@@ -0,0 +1,18 @@
+// pada saat di klik
+$(".page-scroll").on("click", function (e) {
+  //ambil isi href
+  var tujuan = $(this).attr("href");
+  // tangkap elemen ybs
+  var elemenTujuan = $(tujuan);
+
+  // pindahkan scroll
+  $("html, body").animate(
+    {
+      scrollTop: elemenTujuan.offset().top - 60,
+    },
+    0.3,
+    "swing"
+  );
+
+  e.preventDefault();
+});
